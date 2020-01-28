6b1bfea08936eb4d2a142b215fd8f49ff97fb0a5
Andrzej Komarnicki <andrzej_komarnicki@hotmail.com>
2020-01-28 05:59:05 -0500

    Added fine-grained VPC config and VPC endpoints for S3/DynamoDB

--- lib/eks-fargate-cluster-stack.ts
+++ lib/eks-fargate-cluster-stack.ts
@@ -2,10 +2,50 @@ import * as cdk from "@aws-cdk/core";
 import ec2 = require("@aws-cdk/aws-ec2");
 import eks = require("@aws-cdk/aws-eks");
 import iam = require("@aws-cdk/aws-iam");
+import { Vpc, PublicSubnet } from "@aws-cdk/aws-ec2";
 
 export class EksFargateClusterStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    // Custom VPC Build (Min 2 AZs)
+
+    const vpc = new ec2.Vpc(this, "MyVpc", {
+      maxAzs: 2,
+      natGateways: 2,
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: "Public1",
+          subnetType: ec2.SubnetType.PUBLIC
+        },
+        {
+          cidrMask: 24,
+          name: "Public2",
+          subnetType: ec2.SubnetType.PUBLIC
+        },
+        {
+          cidrMask: 24,
+          name: "Private1",
+          subnetType: ec2.SubnetType.PRIVATE
+        },
+        {
+          cidrMask: 24,
+          name: "Private2",
+          subnetType: ec2.SubnetType.PRIVATE
+        }
+      ]
+    });
+
+    // VPC Endpoints
+
+    const DynamoDBEndpoint = vpc.addGatewayEndpoint("DynamoDBEndpoint", {
+      service: ec2.GatewayVpcEndpointAwsService.DYNAMODB
+    });
+
+    const S3Endpoint = vpc.addGatewayEndpoint("S3Endpoint", {
+      service: ec2.GatewayVpcEndpointAwsService.S3
+    });
 
     // allow all account users to assume this role in order to admin the cluster
     const clusterAdmin = new iam.Role(this, "AdminRole", {
@@ -14,7 +54,12 @@ export class EksFargateClusterStack extends cdk.Stack {
 
     // Initialize cluster
     const cluster = new eks.FargateCluster(this, "MyCluster", {
-      mastersRole: clusterAdmin
+      mastersRole: clusterAdmin,
+      outputClusterName: true,
+      outputMastersRoleArn: true,
+      vpc
     });
+
+    // cluster.addResource("HelloApp", ...hello.resources); ?
   }
 }
