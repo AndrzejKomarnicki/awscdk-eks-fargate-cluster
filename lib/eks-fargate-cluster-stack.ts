a6aa401cd9ea3aeebbcd45ee7c679cb8d137ffb2
Andrzej Komarnicki <andrzej_komarnicki@hotmail.com>
2021-03-29 23:36:13 -0400

    upgraded cdk to 1.95.1, replaced unsupported addResource method with addManifest method for ExpressJS deployment

--- lib/eks-fargate-cluster-stack.ts
+++ lib/eks-fargate-cluster-stack.ts
@@ -2,7 +2,7 @@ import * as cdk from "@aws-cdk/core";
 import ec2 = require("@aws-cdk/aws-ec2");
 import eks = require("@aws-cdk/aws-eks");
 import iam = require("@aws-cdk/aws-iam");
-import * as hello from "../expressjs-api/hello-expressjs";
+import { KubernetesVersion } from "@aws-cdk/aws-eks";
 
 export class EksFargateClusterStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -52,18 +52,53 @@ export class EksFargateClusterStack extends cdk.Stack {
     */
 
     // allow all account users to assume this role in order to admin the cluster
-    const clusterAdmin = new iam.Role(this, "AdminRole", {
+    const clusterAdmin = new iam.Role(this, 'AdminRole', {
       assumedBy: new iam.AccountRootPrincipal()
     });
 
     // Initialize cluster
-    const cluster = new eks.FargateCluster(this, "MyCluster", {
+    const cluster = new eks.FargateCluster(this, 'MyCluster', {
+      version: eks.KubernetesVersion.V1_19,
       mastersRole: clusterAdmin,
       outputClusterName: true,
       outputMastersRoleArn: true,
       vpc
     });
 
-    cluster.addResource("Express Hello App", ...hello.resources);
+
+    cluster.addManifest('Express Hello App', {
+      apiVersion: "v1",
+      kind: "Service",
+      metadata: { name: "hello-expressjs" },
+      spec: {
+        type: "NodePort",
+        ports: [{ port: 8080, nodePort: 30036 }],
+        selector: { app: "hello-expressjs" }
+      }
+    },
+      {
+        apiVersion: "apps/v1",
+        kind: "Deployment",
+        metadata: { name: "hello-expressjs" },
+        spec: {
+          replicas: 1,
+          selector: { matchLabels: { app: "hello-expressjs" } },
+          template: {
+            metadata: {
+              labels: { app: "hello-expressjs" }
+            },
+            spec: {
+              containers: [
+                {
+                  name: "hello-expressjs",
+                  image: "andrzejkomarnicki/expresshello:1.0",
+                  ports: [{ containerPort: 8080 }]
+                }
+              ]
+            }
+          }
+        }
+      });
+    
   }
 }
